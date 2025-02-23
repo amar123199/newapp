0d92f7a85ed165d176ad1e0a14e1b701e07ca619
amar123199 <amarsinghamar1121@gmail.com>
2025-02-23 08:57:13 +0530

    experimental added

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Add this line for static exports
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'], // Enable package optimization for Chakra UI
+  },
 };
 
 export default nextConfig;
