24e3a6ff5b406948b529f16e2d7b42ce1be4f168
amar123199 <amarsinghamar1121@gmail.com>
2025-02-23 09:41:52 +0530

    colormode

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //output: 'export', // Add this line for static exports
+  output: 'export', // Add this line for static exports
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'], // Enable package optimization for Chakra UI
   },
