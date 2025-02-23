1e53b32df62c6cb7c5a7526d2a4766a9c23852fc
amar123199 <amarsinghamar1121@gmail.com>
2025-02-23 09:48:28 +0530

    sdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Provider } from "../components/ui/provider"
-
+import "@chakra-ui/react/dist/chakra.css";
 export default function App({ Component, pageProps }) {
   return (
     <Provider>
