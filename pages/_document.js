3912134852164312b8d0c80869773484fa5bccff
amar123199 <amarsinghamar1121@gmail.com>
2025-02-23 08:50:19 +0530

    chakra ui

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { colorModeScript } from '@chakra-ui/react';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head />
       <body>
+      {colorModeScript()} {/* This is important for Chakra UI */}
         <Main />
         <NextScript />
       </body>
